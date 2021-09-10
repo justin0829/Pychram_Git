f7f7cd3d4885c3352e8adf672fc0a98439aa4d11
yaohaoou <yhogz001092@163.com>
2021-09-10 16:36:13 +0800

    Hello_yao

---
+++ 迭代器与生成器/迭代器.py
@@ -0,0 +1,16 @@
+#！/usr/local/bin/python3
+# _*_ coding:utf-8 _*_
+
+"""
+@Time     :  2021/9/10 16:31
+@Author   :  Justin Yao
+@Email    :  yaogz001092@163.com
+@Project  :  Pychram_Git
+@File     :  迭代器.py
+@Software :  PyCharm
+"""
+
+list = [1 , 2 , 3 , 4]
+it = iter(list)
+for x in (list):
+    print(x , end=" ")
