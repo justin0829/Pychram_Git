ae9593006e8ab5e6b027a6884f80ce7fa092f8de
yaohaoou <yhogz001092@163.com>
2021-09-10 16:24:57 +0800

    Hello_yao

---
+++ Test/__init__.py
@@ -0,0 +1,12 @@
+#！/usr/local/bin/python3
+# _*_ coding:utf-8 _*_
+
+"""
+@Time     :  2021/9/10 16:23
+@Author   :  Justin Yao
+@Email    :  yaogz001092@163.com
+@Project  :  Pychram_Git
+@File     :  __init__.py.py
+@Software :  PyCharm
+"""
+$END$
---
+++ Test/hello_world.py
@@ -0,0 +1,12 @@
+#！/usr/local/bin/python3
+# _*_ coding:utf-8 _*_
+
+"""
+@Time     :  2021/9/10 16:24
+@Author   :  Justin Yao
+@Email    :  yaogz001092@163.com
+@Project  :  Pychram_Git
+@File     :  hello_world.py
+@Software :  PyCharm
+"""
+print("Hello World!!!")
